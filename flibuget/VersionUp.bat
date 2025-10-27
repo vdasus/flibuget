13c8019060ffd813e0d4d01f39456650724fb195
vdasus <me@vdasus.com>
2025-10-27 20:15:01 +0200

    Add versioning support and update project metadata
    Added `*.~v` to `.gitignore` to ignore temporary files. Updated `VersionUp.bat` to automate version number updates using `vrinc.exe`. Added `AssemblyVersion` and `FileVersion` properties (set to `0.0.0.4`) in `flibuget`, `flibuget.Core`, and `flibuget.Desktop` project files. Updated `flibuget.slnx` to include a new `/ExtBinaries/` folder with `VersionUp.bat` and `vrinc.exe`. Added the `vrinc.exe` binary file.

---
+++ flibuget/VersionUp.bat
@@ -0,0 +1,3 @@
+vrinc.exe -r -m0001 -fflibuget\flibuget.csproj -t"Version>(\d+)\.(\d+)\.(\d+)\.(\d+)</"
+vrinc.exe -r -m0001 -fflibuget.core\flibuget.Core.csproj -t"Version>(\d+)\.(\d+)\.(\d+)\.(\d+)</"
+vrinc.exe -r -m0001 -fflibuget.Desktop\flibuget.Desktop.csproj -t"Version>(\d+)\.(\d+)\.(\d+)\.(\d+)</"
