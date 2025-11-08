9c1022ce9598c5cf9bb81bcc5e278b4b49f2c79e
vdasus <me@vdasus.com>
2025-11-08 17:02:52 +0200

    Update versions and enhance MainWindow title display
    Updated `AssemblyVersion` and `FileVersion` in `flibuget.Core.csproj`, `flibuget.Desktop.csproj`, and `flibuget.csproj` to reflect new builds.
    
    Enhanced `MainWindow.axaml.cs` to dynamically set the window title with the application name and version, including a distinction for debug builds. The version is retrieved programmatically from the assembly metadata.

--- flibuget/flibuget/Views/MainWindow.axaml.cs
+++ flibuget/flibuget/Views/MainWindow.axaml.cs
@@ -1,13 +1,22 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System.Reflection;
 
 namespace flibuget.Views;
 
 public partial class MainWindow : Window
 {
+    private const string APP_NAME = "flibuget";
+
     public MainWindow()
     {
         InitializeComponent();
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+#if DEBUG
+        Title = $"{APP_NAME} v{version}d";
+#else
+        Title = $"{APP_NAME} v{version}";
+#endif
     }
 
     private void InitializeComponent()
